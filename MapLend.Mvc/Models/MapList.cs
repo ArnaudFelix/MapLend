dc817982dc6dac0899772fefd284d6388198e2cb
ArnaudFelix <arnaud.felix@gmail.com>
2016-07-13 15:47:50 +0200

    jhuj

---
+++ MapLend.Mvc/Models/MapList.cs
@@ -0,0 +1,14 @@
+﻿using MapLend.Business;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MapLend.Mvc.Models
+{
+    public class MapList
+    {
+        public IEnumerable<Map> MyMaps { get; set; }
+        public IEnumerable<Map> ReachMaps { get; set; }
+    }
+}
---
+++ MapLend.Mvc/Models/MapsController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MapLend.Business;
+using MapLend.Mvc.Infrastructure;
+
+namespace MapLend.Mvc.Models
+{
+    [Authorize]
+    public class MapsController : MapControllerBase
+    {
+        // GET: Maps
+        public ActionResult Index()
+        {            
+            MapList maps = new MapList();
+            maps.MyMaps = DbCtx.Maps.Where(m => m.Users.Any(u => u.Id == CurrentUser.Id));
+            maps.ReachMaps = DbCtx.Maps.Where(m => m.Address.ZipCode == CurrentUser.Address.ZipCode).Except(maps.MyMaps);
+            return View(maps);
+        }
+
+
+        // GET: Maps/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Map map = db.Maps.Find(id);
+            if (map == null)
+            {
+                return HttpNotFound();
+            }
+            return View(map);
+        }
+
+        // GET: Maps/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Maps/Create
+        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
+        // plus de détails, voir  http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name")] Map map)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Maps.Add(map);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(map);
+        }
+
+        // GET: Maps/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Map map = db.Maps.Find(id);
+            if (map == null)
+            {
+                return HttpNotFound();
+            }
+            return View(map);
+        }
+
+        // POST: Maps/Edit/5
+        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
+        // plus de détails, voir  http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name")] Map map)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(map).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(map);
+        }
+
+        // GET: Maps/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Map map = db.Maps.Find(id);
+            if (map == null)
+            {
+                return HttpNotFound();
+            }
+            return View(map);
+        }
+
+        // POST: Maps/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Map map = db.Maps.Find(id);
+            db.Maps.Remove(map);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
