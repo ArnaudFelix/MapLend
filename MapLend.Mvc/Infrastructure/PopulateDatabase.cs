926dff78416d28f5efdfa8979350699778687b5c
AstonFrancois <ftibo@free.fr>
2016-07-28 19:27:11 +0200

    .

---
+++ MapLend.Mvc/Infrastructure/PopulateDatabase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MapLend.Mvc.Infrastructure
+{
+    public static class PopulateDatabase
+    {
+    }
+}
